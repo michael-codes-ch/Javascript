3035a840f5ff4ad07d98fcfd5d3797e92e19e23f
michael-codes-ch <chibuokemnwosu@gmail.com>
2026-04-01 08:29:43 -0500

    More exercises

---
+++ javascript.js
@@ -0,0 +1,14 @@
+let age = 16;
+
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23+97);
+console.log(22+3+98+738+22+42);
+console.log((4+6+9)/77);
+
